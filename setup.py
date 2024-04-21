c196336afbf6c9844e2532694123cbe183691203
My-Tien Nguyen <my-tien@mails.taipei>
2024-04-21 20:48:45 +0200

    Update email address in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
 	name = 'recipe_site_generator',
 	description = 'Generates browser documents from input recipe files',
 	author = 'My-Tien Nguyen',
-	author_email = 'my-tien@mailbox.org',
+	author_email = 'my-tien@mails.taipei',
 	url = 'https://github.com/my-tien/recipe_site_generator',
 	packages = find_packages(),
 	install_requires = ('tabulate', 'pyyaml'),
